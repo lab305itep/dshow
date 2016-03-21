998f213f0f8bcc6155bddbc15e412ba676fe3c38
Igor Alekseev <igor.alekseev@itep.ru>
2016-03-21 21:13:55 +0300

    Working on channel mapping

--- danal2.cpp
+++ danal2.cpp
@@ -7,6 +7,18 @@
 
 #define MAXHIT 500
 #define ESIZE (sizeof(struct pre_event_struct) + MAXHIT * sizeof(struct pre_hit_struct))
+#define MAXWFD 70
+
+struct chan_def_struct {
+	char type;
+	char proj;
+	char xy;
+	char z;
+	float dt;
+	float ecoef;
+};
+
+struct chan_def_struct Map[MAXWFD][64];
 
 TFile *OpenRootFile(const char *fname)
 {
@@ -18,6 +30,52 @@ TFile *OpenRootFile(const char *fname)
 	if (ptr) *ptr = '\0';
 	strcat(outname, ".root");
 	return new TFile(outname, "RECREATE");
+}
+
+int ReadMap(const char *fname)
+{
+	FILE *f;
+	int type;
+	int proj;
+	int xy;
+	int z;
+	float dt;
+	float ecoef;
+	int mod, chan;
+	int cnt;
+
+	memset(Map, -1, sizeof(Map));
+	f = fopen(fname, "rt");
+	if (!f) {
+		printf("Can not open map-file %s: %m\n", fname);
+		return -10;
+	}
+	cnt = 0;
+	while (!feof(f)) {
+//		mod chan type proj xy z dt ecoef
+		fscanf(f, "%d %d %d %d %d %d %f %f\n", &mod, &chan, &type, &proj, &xy, &z, &dt, &ecoef);
+		if (mod < 1 || mod > MAXWFD) {
+			printf("Module number = %d out of range: 1 - %d\n", mod, MAXWFD);
+			return -20;
+		}
+		if (chan < 0 || chan > 63) {
+			printf("Channel number = %d out of range: 0 - 63\n", chan);
+			return -20;
+		}
+		if (Map[mod-1][chan].type != -1) {
+			printf("Duplicated channel found: %d.%2.2d\n", mod, chan);
+			return -30;
+		}
+		Map[mod-1][chan].type  = type;
+		Map[mod-1][chan].proj  = proj;
+		Map[mod-1][chan].xy    = xy;
+		Map[mod-1][chan].z     = z;
+		Map[mod-1][chan].dt    = dt;
+		Map[mod-1][chan].ecoef = ecoef;
+		cnt++;
+	}
+	fclose(f);
+	printf("%s: %d channels found\n", fname, cnt);
 }
 
 int main(int argc, char**argv)
@@ -35,6 +93,8 @@ int main(int argc, char**argv)
 	fIn = NULL;
 	fOut = NULL;
 	oldtime = -125000000;
+
+	if (ReadMap("danss_map.txt")) return 10;
 
 //		Initializing files and memory
 	if (argc < 1) {
@@ -79,7 +139,7 @@ int main(int argc, char**argv)
 		oldtime = event->gtime;
 		A = 0;
 		for (i=0; i<event->nhits; i++) if (event->hit[i].mod != 1 && event->hit[i].mod != 3 && event->hit[i].amp > A) A = event->hit[i].amp;
-		hMaxSi->Fill(A);
+		hMaxSi->Fill(A / 250.0);
 	}
 
 fin:
